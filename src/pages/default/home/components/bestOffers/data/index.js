b18b5a8bf99233e6ec4cc91eea6aaee9313ca604
Ural <shaxob0211@gmail.com>
2025-01-27 16:21:28 +0500

    Created 2 page

---
+++ src/pages/default/home/components/bestOffers/data/index.js
@@ -0,0 +1,76 @@
+const productData = [
+	{
+		name: "Rapeseed table PRZ 7 m, for Gomselmash reapers",
+		shortDescription: "Headers and attachments",
+		price: "1300390000",
+		discount: "12",
+		imageLink: "https://s7d9.scene7.com/is/image/agco/MF9895_IMG_5433_1400x700-1",
+	},
+	{
+		name: "BWC (black-and-white cows)",
+		shortDescription: "Black and white breed",
+		price: "40000000",
+		discount: "9",
+		imageLink: "https://c02.purpledshub.com/uploads/sites/47/2024/01/black-and-white-cow-breeds.jpg",
+	},
+	{
+		name: "Grain seeder 2BXFZ-12",
+		shortDescription: "Seeders",
+		price: "50000000",
+		discount: "15",
+		imageLink: "https://traktor-shop.ru/upload/iblock/738/8v7tdzuv4cyl83n3bl060dg19td2kz74/IMG_1271.jpg",
+	},
+	{
+		name: "Plow P-5",
+		shortDescription: "Soil tillage equipment",
+		price: "25000000",
+		discount: "10",
+		imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMAnqFZgB3dE1qc-INF5GtZ3hEThE32sjGrw&s",
+	},
+	{
+		name: "Corn harvester CH-12",
+		shortDescription: "Harvesters",
+		price: "900000000",
+		discount: "13",
+		imageLink:
+			"https://www.deere.ca/assets/images/region-4/products/harvesting/corn-heads/cf12/r4k033425_rrd_v2_1024x576_large_941fef27514881c4690f76b47c4908b5178426f1.jpg",
+	},
+	{
+		name: "Transmission oil Shell Spirax S4 TXM barrel, 209 l",
+		shortDescription: "Transmission oil",
+		price: "10000000",
+		discount: "10",
+		imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROLVGZpa1mGdCLrmmKz4a2xq3wPZdEKQiJ4w&s",
+	},
+	{
+		name: "Karabayr horses",
+		shortDescription: "Horses",
+		price: "335000000",
+		discount: "12",
+		imageLink:
+			"https://iadsb.tmgrup.com.tr/7ddb86/0/0/0/0/1926/1086?u=https://idsb.tmgrup.com.tr/2018/05/22/horses-the-wings-of-mankind-1527015927739.jpg",
+	},
+	{
+		name: "Organic Non-GMO Natural Sweet XR F1 Sweet Corn Seeds",
+		shortDescription: "Seeds",
+		price: "500000",
+		discount: "10",
+		imageLink: "https://m.media-amazon.com/images/I/61mYbBZ+deL.jpg",
+	},
+	{
+		name: "Rotary tiller RT-20",
+		shortDescription: "agricultural machinery",
+		price: "8800000",
+		discount: "18",
+		imageLink: "https://mar-mot.ge/wp-content/uploads/2023/08/1gqn-200.png",
+	},
+	{
+		name: "Seed cleaning machine SC-15",
+		shortDescription: "Seed processing",
+		price: "65000000",
+		discount: "13",
+		imageLink: "https://s.alicdn.com/@sc04/kf/H5d8abc5c333041238d2674a5a8104fefj.jpg_300x300.jpg",
+	},
+];
+
+export default productData;
---
+++ src/pages/default/home/components/yourMedia/data/catalog.js
@@ -0,0 +1,116 @@
+const data = [
+	{
+		id: 101,
+		name: "All",
+		children: [
+			{
+				id: 1,
+				name: "State",
+				title: "State support for the agro-industrial complex will be linked to the Central Bank's key rate",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737628537_shutterstock_2495959483.jpg",
+			},
+			{
+				id: 2,
+				name: "animals",
+				title: "A Holistic View of Plant Growing: Noospheric Principles in Commodity Business",
+				date: "01.03.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737622991_shutterstock_2309059777.jpg",
+			},
+			{
+				id: 3,
+				name: "animals",
+				title: "Agritourism as a driver of rural development",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737616019_shutterstock_2172821927.jpg",
+			},
+		],
+	},
+	{
+		id: 102,
+		name: "Articles",
+		children: [
+			{
+				id: 1,
+				name: "State",
+				title: "State support for the agro-industrial complex will be linked to the Central Bank's key rate",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737628537_shutterstock_2495959483.jpg",
+			},
+			{
+				id: 2,
+				name: "animals",
+				title: "A Holistic View of Plant Growing: Noospheric Principles in Commodity Business",
+				date: "01.03.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737622991_shutterstock_2309059777.jpg",
+			},
+		],
+	},
+	{
+		id: 103,
+		name: "News",
+		children: [
+			{
+				id: 1,
+				name: "State",
+				title: "State support for the agro-industrial complex will be linked to the Central Bank's key rate",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737628537_shutterstock_2495959483.jpg",
+			},
+			{
+				id: 2,
+				name: "animals",
+				title: "A Holistic View of Plant Growing: Noospheric Principles in Commodity Business",
+				date: "01.03.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737622991_shutterstock_2309059777.jpg",
+			},
+		],
+	},
+	{
+		id: 104,
+		name: "Video",
+		children: [
+			{
+				id: 1,
+				name: "State",
+				title: "State support for the agro-industrial complex will be linked to the Central Bank's key rate",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737628537_shutterstock_2495959483.jpg",
+				isVideo: true,
+			},
+			{
+				id: 2,
+				name: "animals",
+				title: "A Holistic View of Plant Growing: Noospheric Principles in Commodity Business",
+				date: "01.03.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737622991_shutterstock_2309059777.jpg",
+
+				isVideo: true,
+			},
+		],
+	},
+	{
+		id: 105,
+		name: "Online broadcasts",
+		children: [
+			{
+				id: 1,
+				name: "Live",
+				title: "State support for the agro-industrial complex will be linked to the Central Bank's key rate",
+				date: "12.02.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737628537_shutterstock_2495959483.jpg",
+				isVideo: true,
+			},
+			{
+				id: 2,
+				name: "Live",
+				title: "A Holistic View of Plant Growing: Noospheric Principles in Commodity Business",
+				date: "01.03.2025",
+				img: "https://svoefermerstvo.ru/img/0/0/resize/rshb/agrolife/1737622991_shutterstock_2309059777.jpg",
+				isVideo: true,
+			},
+		],
+	},
+];
+
+export default data;
