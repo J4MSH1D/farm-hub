bad9e96b42377442e41761e33c64d32231f010c5
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2025-01-31 19:12:31 +0500

    Fixing UI

--- src/pages/default/home/components/services/index.vue
+++ src/pages/default/home/components/services/index.vue
@@ -9,59 +9,58 @@ const modules = [Navigation];
 const activeTab = ref("Sales");
 
 const tabsList = [
-	{ name: "Sales of products", value: "Sales" },
-	{ name: "Bank Services", value: "Bank" },
-	{ name: "Preferential lending", value: "Lending" },
-	{ name: "For Beginning Farmers", value: "Beginning" },
-	{ name: "Promotion in marketplaces", value: "Promotion" },
+  { name: "Сбыт продукции", value: "Sales" },
+  { name: "Услуги банка", value: "Bank" },
+  { name: "Льготное кредитование", value: "Lending" },
+  { name: "Начинающим своё дело", value: "Beginning" },
 ];
 </script>
 <template>
-	<div class="container bg-white py-10 rounded-xl my-10 relative">
-		<div class="flex justify-between items-start relative z-30">
-			<div>
-				<div class="text-3xl font-medium">Services for your business</div>
-				<div class="mt-5 text-xl text-gray-400 max-w-[900px]">
-					We have collected the best industry solutions that will help your business reach the next level
-				</div>
-			</div>
-			<div>
-				<rightArrowButton color="#4AB75F" />
-			</div>
-		</div>
-		<div class="mt-5 relative z-30">
-			<div class="flex gap-x-3">
-				<span
-					class="bg-[#ECEDEF] px-4 select-none py-2 rounded-5xl text-gray-400 cursor-pointer transition"
-					v-for="tab in tabsList"
-					:class="{ 'bg-black !text-white': tab.value === activeTab }"
-					@click="activeTab = tab.value"
-				>
-					{{ tab.name }}
-				</span>
-			</div>
-		</div>
-		<div class="mt-5 relative z-30">
-			<swiper :slidesPerView="'auto'" :navigation="true" :modules="modules" :spaceBetween="30" class="mySwiper">
-				<template v-for="link in tabsLinks.find(e => e.value === activeTab)['children']">
-					<swiper-slide class="!w-[250px] min-h-[150px] bg-gray-100 p-4 rounded-4xl cursor-pointer">
-						<div class="w-full min-h-[50px] mb-5 flex justify-end items-start">
-							<template v-if="link.icon">
-								<div class="w-12 aspect-square rounded-full flex justify-center items-center" :style="{ backgroundColor: link.color }">
-									<icon :name="link.icon" is-svg />
-								</div>
-							</template>
-						</div>
-						<div>{{ link.name }}</div>
-					</swiper-slide>
-				</template>
-			</swiper>
-		</div>
-		<icon name="cms.png" class="absolute h-1/2 top-10 right-[10%] z-0" />
-	</div>
+  <div class="container bg-white py-10 rounded-xl my-10 relative">
+    <div class="flex justify-between items-start relative z-30">
+      <div>
+        <div class="text-3xl font-medium">Услуги для вашего бизнеса</div>
+        <div class="mt-5 text-xl text-gray-400 max-w-[900px]">
+          Мы собрали лучшие отраслевые решения, которые помогут вашему бизнесу выйти на новый уровень
+        </div>
+      </div>
+      <div>
+        <rightArrowButton color="#4AB75F" />
+      </div>
+    </div>
+    <div class="mt-5 relative z-30">
+      <div class="flex gap-x-3">
+        <span
+          class="bg-[#ECEDEF] px-4 select-none py-2 rounded-5xl text-gray-400 cursor-pointer transition"
+          v-for="tab in tabsList"
+          :class="{ 'bg-black !text-white': tab.value === activeTab }"
+          @click="activeTab = tab.value"
+        >
+          {{ tab.name }}
+        </span>
+      </div>
+    </div>
+    <div class="mt-5 relative z-30">
+      <swiper :slidesPerView="'auto'" :navigation="true" :modules="modules" :spaceBetween="30" class="mySwiper">
+        <template v-for="link in tabsLinks.find(e => e.value === activeTab)['children']">
+          <swiper-slide class="!w-[250px] min-h-[150px] bg-gray-100 p-4 rounded-4xl cursor-pointer">
+            <div class="w-full min-h-[50px] mb-5 flex justify-end items-start">
+              <template v-if="link.icon">
+                <div class="w-12 aspect-square rounded-full flex justify-center items-center" :style="{ backgroundColor: link.color }">
+                  <icon :name="link.icon" is-svg />
+                </div>
+              </template>
+            </div>
+            <div>{{ link.name }}</div>
+          </swiper-slide>
+        </template>
+      </swiper>
+    </div>
+    <icon name="cms.png" class="absolute h-1/2 top-10 right-[10%] z-0" />
+  </div>
 </template>
 <style>
 .swiper-button-disabled {
-	display: none;
+  display: none;
 }
 </style>
--- src/pages/default/home/components/startingBusiness/index.vue
+++ src/pages/default/home/components/startingBusiness/index.vue
@@ -1,23 +1,23 @@
 <template>
-	<div class="container my-10 bg-white py-10 rounded-xl">
-		<div class="px-10 py-8 rounded-4xl background relative z-40 overflow-hidden">
-			<div class="relative z-20 text-white flex justify-between items-center">
-				<div>
-					<div class="text-3xl">How to start your own business from scratch</div>
-					<div class="mt-2">Popular directions and examples of business plans</div>
-				</div>
-				<div>
-					<arrowButton color="#4481eb"></arrowButton>
-				</div>
-			</div>
-			<icon name="rocket.png" class="absolute right-[20%] top-[-10px] h-[150%] z-10" />
-			<icon name="gradient" is-svg class="absolute right-0 top-0 w-1/2 h-full object-cover z-0" />
-		</div>
-	</div>
+  <div class="container my-10 bg-white py-10 rounded-xl">
+    <div class="px-10 py-8 rounded-4xl background relative z-40 overflow-hidden">
+      <div class="relative z-20 text-white flex justify-between items-center">
+        <div>
+          <div class="text-3xl">Как начать свой бизнес с нуля</div>
+          <div class="mt-2">Бизнес-план по выращиванию фруктов и овощей</div>
+        </div>
+        <div>
+          <arrowButton color="#4481eb"></arrowButton>
+        </div>
+      </div>
+      <icon name="rocket.png" class="absolute right-[20%] top-[-10px] h-[150%] z-10" />
+      <icon name="gradient" is-svg class="absolute right-0 top-0 w-1/2 h-full object-cover z-0" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .background {
-	background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
+  background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
 }
 </style>
