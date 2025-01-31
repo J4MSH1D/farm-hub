8eaccc8be7549b499f661f06d4b0047561060817
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2025-01-31 20:17:24 +0500

    Fixing UI

--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -25,7 +25,7 @@ async function sign() {
     // storage.set("accessToken", JSON.stringify(storedData));
     // router.push("/profile/root");
   } catch {
-    message.error("Error signing with EIMZO key");
+    message.error("Ошибка подписи с ключом E-IMZO");
   }
 }
 
@@ -37,20 +37,20 @@ onMounted(async () => {
   <div class="container my-40 grid grid-cols-2 gap-30">
     <div>
       <div class="text-center">
-        <div class="font-medium text-xl">Authorization in the services of BRB Farm-Hub</div>
-        <div class="mt-5 font-bold text-3xl">Run Agribusiness on <span class="font-bold text-3xl text-mainGreen">Your Own Farm</span></div>
-        <div class="mt-5 text-gray-400">Manage several personal accounts at once: Your Farming, Your Home, Work</div>
+        <div class="font-medium text-xl">Авторизация в сервисах</div>
+        <div class="mt-5 font-bold text-3xl">Управляйте агробизнесом <span class="font-bold text-3xl text-mainGreen">на своей ферме</span></div>
+        <div class="mt-5 text-gray-400">Управляйте несколькими личными аккаунтами одновременно: «Ваше хозяйство», «Ваш дом», «Работа».</div>
       </div>
     </div>
     <div class="bg-white rounded-3xl py-10 px-20">
-      <div class="text-center font-medium text-2xl">Login to your personal account</div>
+      <div class="text-center font-medium text-2xl">Войдите в личный кабинет</div>
       <div class="my-5">
-        <a-select v-model:value="selectedValue" class="w-full" size="large" placeholder="Choose the right EIMZO key">
+        <a-select v-model:value="selectedValue" class="w-full" size="large" placeholder="Выберите ключ E-IMZO">
           <a-select-option v-for="key in keys" :value="key['esiId']">{{ key["CN"] }}</a-select-option>
         </a-select>
       </div>
       <div class="my-5">
-        <a-button size="large" @click="sign" class="min-w-full" type="primary">Login</a-button>
+        <a-button size="large" @click="sign" class="min-w-full" type="primary">Авторизоваться</a-button>
       </div>
     </div>
   </div>
