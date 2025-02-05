c63b3f184eb49ae32c499019ba0ed1208d1c7878
Ravshan Akhadov <ravshansobirovich94@gmail.com>
2025-02-05 11:30:53 +0500

    ref

--- src/pages/dashboard/main/index.vue
+++ src/pages/dashboard/main/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+import storage from "@/utils/storageService";
+
+const user = storage.get("accessToken") ? JSON.parse(storage.get("accessToken")) : null;
+console.log(user);
+</script>
 <template>
-  <div>Dashboard</div>
+  <div class="flex-1 px-8 py-6">
+    <BannerMain />
+  </div>
 </template>
--- src/pages/dashboard/profile/root/index.vue
+++ src/pages/dashboard/profile/root/index.vue
@@ -1,3 +1,9 @@
+<script setup>
+import storage from "@/utils/storageService";
+
+const user = storage.get("accessToken") ? JSON.parse(storage.get("accessToken")) : null;
+console.log(user);
+</script>
 <template>
-	<div>Profile</div>
+  <div>Profile</div>
 </template>
