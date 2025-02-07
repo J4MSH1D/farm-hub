4dcf3037f0e010a2629127cc0ea1d4bf9dc14e22
Ravshan Akhadov <ravshansobirovich94@gmail.com>
2025-02-08 02:26:05 +0500

    123

--- src/pages/dashboard/translations/index.vue
+++ src/pages/dashboard/translations/index.vue
@@ -1,3 +1,39 @@
+<script setup>
+import { computed } from "vue";
+import axios from "axios";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: true,
+    width: "20%",
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    filters: [
+      { text: "Male", value: "male" },
+      { text: "Female", value: "female" },
+    ],
+    width: "20%",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+];
+</script>
 <template>
-  <div class="container">Translations</div>
+  <a-table
+    :columns="columns"
+    :row-key="record => record.login.uuid"
+    :data-source="dataSource"
+    :pagination="pagination"
+    :loading="loading"
+    @change="handleTableChange"
+  >
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">{{ text.first }} {{ text.last }}</template>
+    </template>
+  </a-table>
 </template>
