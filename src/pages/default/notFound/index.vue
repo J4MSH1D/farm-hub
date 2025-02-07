4cf669fa971b6030a684f9a4c7a8a1bc39222080
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2025-02-08 00:44:19 +0500

    Fixing UI

--- src/pages/default/notFound/index.vue
+++ src/pages/default/notFound/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container my-20 bg-white py-30 rounded-4xl flex justify-center items-center">
-		<span class="text-5xl">Not Found</span>
-	</div>
+  <div class="container my-20 bg-white py-30 rounded-4xl flex justify-center items-center">
+    <span class="text-5xl">{{ $t("Нет данных") }}</span>
+  </div>
 </template>
--- src/pages/default/services/components/Banner.vue
+++ src/pages/default/services/components/Banner.vue
@@ -2,8 +2,8 @@
   <div class="relative mt-10 overflow-hidden rounded-3xl">
     <banner backgroundColor="services">
       <div class="relative z-20 flex flex-col gap-4 min-h-[220px]">
-        <h2 class="text-xl lg:text-3xl font-medium text-white leading-10">Ваше хозяйство — наша забота</h2>
-        <p class="text-lg text-white">Полный спектр услуг для вашего участка, оптимизируйте работу своего хозяйства с нашими услугами</p>
+        <h2 class="text-xl lg:text-3xl font-medium text-white leading-10">{{ $t("Ваше хозяйство — наша забота") }}</h2>
+        <p class="text-lg text-white">{{ $t("Полный спектр услуг для вашего участка, оптимизируйте работу своего хозяйства с нашими услугами") }}</p>
       </div>
       <a-button class="text-[#2C6E4A] !rounded-tl-3xl !rounded-br-3xl !rounded-bl-none !rounded-tr-none ml-10" size="large">{{
         $t("Разместить услугу")
